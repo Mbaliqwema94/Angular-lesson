0393ba251d39680c3fd7813d83328d997b7d2251
DA-MAC-M1-109 <da-mac-m1-109@DA-Mac109s-MacBook-Air109.local>
2023-04-19 14:36:41 +0200

    Angular-lesson

---
+++ Angular-lesson/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CartComponent } from './cart/cart.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  {path:'products', component: ProductsComponent},
+  {path:'cart', component: CartComponent},
+  {path:'home', component: HomepageComponent},
+  {path:'', component: NavbarComponent},
+  {path:'footer', component: FooterComponent}
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
